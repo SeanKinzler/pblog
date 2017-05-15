9e6c68b940bbf3109305bd73457f3dc07087e051
seankinzler <sean.kinzler92@gmail.com>
2017-05-15 09:02:04 -0700

    r router v4 not working with async route

--- client/index.jsx
+++ client/index.jsx
@@ -2,21 +2,11 @@ import React, { Component } from 'react';
 // import { browserHistory, DefaultRoute, IndexRoute, Link, Route, RouteHandler, Router} from 'react-router';
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 import { render } from 'react-dom';
-import Home from './components/home.jsx';
-import AddStory from './components/addStory.jsx';
-import Login from './components/login.jsx';
-import jwt from './components/jwt.jsx';
-import { PrivateRoute } from './utils/auth.jsx';
 
+import Routes from './config/routes.jsx';
 render((
   <Router>
-    <div>
-      <Route exact path='/' component={ Home } />
-      <Route path='/failedLogin' component={ Home } />
-      <Route path='/addstory1' component={ AddStory } />
-      <PrivateRoute path='/addstory2' component={ AddStory } />
-      <Route path='/login' component={ Login } />
-      <Route path='/jwt/:token' component={ jwt } />
-    </div>
+    <Routes />
   </Router>
   ), document.getElementById('App'));
+
--- client/utils/auth.jsx
+++ client/utils/auth.jsx
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Link, Route, Redirect} from 'react-router-dom';
+import axios from 'axios';
 
-const PrivateRoute = ({component: Component, ...rest}) => {
-  return (<Route {...rest} render={props => (
-      checkAuth() ? (
-        <Component {...props}/>
-      ) : (
+axios.defaults.headers.common['jwt'] = localStorage.token;
+const PrivateRoute = ({component: Component, that, ...rest}) => {
+  if (!that.state.auth) {
+    checkAuth(that).catch(err => {
+      console.log(err);
+      return (<Route {...rest} render={props => (
         <Redirect to={{pathname: '/'}} />
-      )
-    )}
-  />)
+        )} />)
+    });
+  }
+  console.log('if check: ', that.state.auth)
+  if (that.state.auth) {
+    return (<Route {...rest} render={props => (
+      <Component {...props} />
+      )} />)
+  } else {
+    return (<Route {...rest} render={props => (
+      <Redirect to={{pathname: '/'}} />
+      )} />)
+  }
 };
 
-const checkAuth = (callback) => {
-  console.log('checkAuth');
-  return true;
+const checkAuth = (that) => {
+  return axios.post('/auth/verify').then((res) => {
+    if (res) {
+      console.log('authenticated');
+      localStorage.name = res.data;
+      that.updateAuth(true);
+      // that.forceUpdate();
+      return true;
+    } else {
+      console.log('failed to authenticate');
+      return false;
+    }
+  })
 };
 
 module.exports = {
